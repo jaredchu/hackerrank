5fe7fcaf78885e6911c020a39522a3c8157e8a0a
Jared Chu <me@jaredchu.com>
2018-03-26 11:04:24 +0700

    .

--- 30-days-of-code/x-day-23-binary-trees.py
+++ 30-days-of-code/x-day-23-binary-trees.py
@@ -1,4 +1,4 @@
-import sys
+import itertools
 
 class Node:
     def __init__(self,data):
@@ -17,24 +17,38 @@ class Solution:
                 root.right=cur
         return root
 
+    def zip_longest_nofill(self, *args):
+        empty = object()
+        its = [iter(arg) for arg in args]
+        while True:
+            vals = (next(it, empty) for it in its)
+            tup = tuple(val for val in vals if val is not empty)
+            if not tup:
+                return
+            yield tup
+
     def levelOrder(self,root):
-        print(str(root.data) + " " + self.getLevel(root))
+        level = self.getLevel(root)
+        level.insert(0,root.data)
+        print(" ".join(str(num) for num in level))
 
     def getLevel(self,root):
         if root is None or root.data is None:
-            return ""
+            return []
 
-        level = ""
+        level = []
         if root.left is not None:
-            level += str(root.left.data) + " "
+            level.append(root.left.data)
 
         if root.right is not None:
-            level += str(root.right.data) + " "
+            level.append(root.right.data)
 
-        level += self.getLevel(root.left)
-        level += self.getLevel(root.right)
-
-        return level
+        left = self.getLevel(root.left)
+        right = self.getLevel(root.right)
+        mix = list(itertools.chain.from_iterable(self.zip_longest_nofill(left, right)))
+        print(left)
+        print(right)
+        return level + mix
 
 
 T=int(input())
