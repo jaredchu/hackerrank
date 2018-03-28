5ae59988632e813177f8b4a3e839811bdbe8cb90
Jared Chu <me@jaredchu.com>
2018-03-28 15:53:06 +0700

    .

---
+++ 30-days-of-code/day-25-running-time-and-complexity.py
@@ -0,0 +1,23 @@
+def is_prime(number):
+    if number <= 1:
+        return False
+    elif number <= 3:
+        return True
+    elif number % 2 is 0 or number % 3 is 0:
+        return False
+
+    i = 5
+    while i ** 2 <= number:
+        if number % i is 0 or number % (i + 2) is 0:
+            return False
+        i = i + 6
+    return True
+
+
+n = int(input().strip())
+arr = []
+for _ in range(n):
+    if is_prime(int(input().strip())):
+        print('Prime')
+    else:
+        print('Not prime')
