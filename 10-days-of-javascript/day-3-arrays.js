3869c70855866ca6b46c2aa7bd13d97c904c8af7
Jared Chu <me@jaredchu.com>
2018-04-11 09:43:43 +0700

    .

---
+++ 10-days-of-javascript/day-3-arrays.js
@@ -0,0 +1,49 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+Array.prototype.unique = function() {
+    return this.filter(function (value, index, self) {
+        return self.indexOf(value) === index;
+    });
+};
+
+function sortNumber(a,b) {
+    return a - b;
+}
+
+/**
+ *   Return the second largest number in the array.
+ *   @param {Number[]} nums - An array of numbers.
+ *   @return {Number} The second largest number in the array.
+ **/
+function getSecondLargest(nums) {
+    return nums.unique().sort(sortNumber).reverse()[1];
+}
+
+function main() {
+    const n = +(readLine());
+    const nums = readLine().split(' ').map(Number);
+
+    console.log(getSecondLargest(nums));
+}
